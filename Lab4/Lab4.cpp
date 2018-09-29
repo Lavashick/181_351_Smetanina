22f9ec879889ee7d532e7f025c3671e7ca6439bb
Сметанина Александра Александровна <a.a.smetanina@stud.unv.su>
2018-09-29 15:51:16 +0300

    классная работа 29.09.18

---
+++ Lab4/Lab4.cpp
@@ -0,0 +1,56 @@
+
+#include "stdafx.h"
+
+#include <iostream>
+using namespace std;
+
+// Указатель (pointer)- переменная, хранящая адрес смещений каких-либо данных, переменных в оперативной памяти 
+// Объявляется как обычная переменаая, но добавляется звездочка в начале
+// Фактически указатель- это unsigned или unsigned long long  с адресом, так как дробных адресов нет
+// 
+
+
+
+int main()
+{
+	int a;
+	double dbl;
+
+	
+	int * ptr_int;
+	ptr_int = &a;
+	double * ptr_dbl = &dbl;
+	
+
+	cout << "int pointer size = " << sizeof(ptr_int) << endl;
+	cout << "double pointer size = " << sizeof(ptr_dbl) << endl;
+	cout << "char pointer size = " << sizeof(char *) << endl;
+	cout << "long long pointer size = " << sizeof(long long *) << endl;
+	cout << "bool pointer size = " << sizeof(bool *) << endl;
+
+	
+	// 3. Содержимое указателя
+	cout << "ptr_int = " << ptr_int << endl;
+	cout << "ptr_dbl = " << ptr_dbl << endl;
+
+	// Содержимое указателя 
+	cout << endl << "prt_int = 0x" << ptr_int << endl;
+
+	int arr, arr1D;
+	 
+	cout << "arr =" << arr1D << endl;
+	cout << "*arr = arr[0] = " << *arr1D << endl;
+	cout << "*(arr+4) = arr[4] = " << *(arr1D+4) << endl;
+	
+
+
+	
+	
+	
+	
+	
+	
+
+	return 0;
+}
+
